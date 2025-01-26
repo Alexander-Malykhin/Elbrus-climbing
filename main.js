2a9759641d87e10de2cbd2ba0656079ec86ad3bc
Alexander-Malykhin <well02091999@gmail.com>
2025-01-26 18:35:47 +0300

    version 1.0.0

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
+//BUTTONS ANIMATIONS
 const BUTTON_ORDER = document.querySelector('.header__button');
-const BUTTON_DOWNLOAD = document.querySelector('.program__button-download')
-const BUTTON__PHOTO = document.querySelector('.program__button-photo')
+const BUTTON__PHOTO = document.querySelector('.program__conditions-button')
 const BUTTON_ORDER_ROUTE = document.querySelector('.choice__table-button')
+//BUTTONS MENU
 const BURGER = document.querySelector('.burger')
-const MENU = document.querySelector('.menu')
 const CLOSE_MENU = document.querySelector('.menu__close')
+//BUTTON SELECT
 const SELECT_BUTTON_CURRENCY = document.querySelector('.menu__select-currency')
 const SELECT_BUTTON_LANGUAGE = document.querySelector('.menu__select-language')
-const INFORMATION_ITEMS = document.querySelectorAll('.information__list-item')
-const INFORMATION_BUTTONS = document.querySelectorAll('.information__button-item')
-const INFORMATION_CONTENT = document.querySelectorAll('.information__list-content')
-const SCHEDULE_BUTTONS = document.querySelectorAll('.schedule__button-item')
-const SCHEDULE_ITEMS = document.querySelectorAll('.schedule__list-item')
-const SCHEDULE_CONTENT = document.querySelectorAll('.schedule__list-text')
-const SCHEDULE_BUTTON = document.querySelector('.schedule__button-list')
+//BUTTONS ANIMATIONS
+const buttonOrderClick = (button) => {
+    button.addEventListener('mousedown', () => {
+        button.style.opacity = '0.7';
+    })
 
-
+    button.addEventListener('mouseup', () => {
+        button.style.opacity = '1';
+    })
+}
+buttonOrderClick(BUTTON_ORDER)
+buttonOrderClick(BUTTON__PHOTO)
+buttonOrderClick(BUTTON_ORDER_ROUTE)
+//VIDEO-BACKGROUND
 const videoBackground = () => {
     const VIDEO = document.querySelector('#video')
-    const BUTTON_VIDEO = document.querySelector('.header__information-button')
-    const img = document.querySelector('#btn')
+    const BUTTON_VIDEO = document.querySelector('.header__banner-button')
+    const BUTTON_SHOW = document.querySelector('#button_video')
 
     BUTTON_VIDEO.addEventListener('click', () => {
         if (VIDEO.paused) {
-            img.src = './images/pause.png'
+            BUTTON_SHOW.src = './images/pause.png'
             VIDEO.play()
         } else {
-            img.src = './images/triangle.png'
+            BUTTON_SHOW.src = './images/triangle.png'
             VIDEO.pause()
         }
     })
 }
-
 videoBackground()
-
+//VIDEO SLIDER
 const videoSlider = () => {
     const VIDEO_LIST = document.querySelectorAll('.header__slider-video');
     const BUTTON_VIDEO_LIST = document.querySelectorAll('.header__slider-button');
@@ -63,10 +68,100 @@ const videoSlider = () => {
         });
     });
 };
-
 videoSlider();
+//BUTTON MENU BURGER
+const buttonMenuClick = (button) => {
+    const BACKGROUND_MENU = document.querySelector('.menu__background')
+    const MENU = document.querySelector('.menu')
+
+    if (BACKGROUND_MENU) {
+        BACKGROUND_MENU.addEventListener('click', () => {
+            MENU.classList.remove('menu__show')
+            return document.body.style.overflow = '';
+        })
+    }
+
+    button.addEventListener('click', () => {
+        MENU.classList.toggle('menu__show')
+
+        return MENU.classList.contains('menu__show') ?
+            document.body.style.overflow = 'hidden'
+            :
+            document.body.style.overflow = '';
+    })
+}
+buttonMenuClick(BURGER)
+buttonMenuClick(CLOSE_MENU)
+//SCHEDULE LIST
+const scheduleList = () => {
+    const SCHEDULE_BUTTONS = document.querySelectorAll('.schedule__button-item')
+    const SCHEDULE_ITEMS = document.querySelectorAll('.schedule__list-item')
+    const SCHEDULE_CONTENT = document.querySelectorAll('.schedule__list-text')
+    const SCHEDULE_BUTTON = document.querySelector('.schedule__button-list')
+    //SLIDER
+    const SLIDERS = document.querySelectorAll('.schedule__mobile-item')
+    const BUTTON_LEFT = document.querySelectorAll('.schedule__left')
+    const BUTTON_RIGHT = document.querySelectorAll('.schedule__right')
+    const BUTTON = document.querySelector('.schedule__mobile-list')
 
 
+    SCHEDULE_ITEMS.forEach((element, index) => {
+
+        element.addEventListener('click', () => {
+            if (element) {
+                SCHEDULE_BUTTONS[index].classList.toggle('information__list-button')
+                SCHEDULE_CONTENT[index].classList.toggle('information__show')
+            }
+        })
+    })
+
+    SCHEDULE_BUTTON.addEventListener('click', () => {
+        const BUTTON_CONTENT = SCHEDULE_BUTTON.innerText
+
+        switch (BUTTON_CONTENT) {
+            case 'Раскрыть все дни' :
+                SCHEDULE_BUTTON.innerText = 'Закрыть все дни'
+
+                SCHEDULE_ITEMS.forEach((element, index) => {
+                    SCHEDULE_BUTTONS[index].classList.add('information__list-button')
+                    SCHEDULE_CONTENT[index].classList.add('information__show')
+                })
+                break;
+            case 'Закрыть все дни' :
+                SCHEDULE_BUTTON.innerText = 'Раскрыть все дни'
+
+                SCHEDULE_ITEMS.forEach((element, index) => {
+                    SCHEDULE_BUTTONS[index].classList.remove('information__list-button')
+                    SCHEDULE_CONTENT[index].classList.remove('information__show')
+                })
+                break;
+        }
+    })
+
+    let offset = SLIDERS[0].clientWidth
+    let count = 0
+
+    BUTTON_RIGHT.forEach((element, index) => {
+        BUTTON_RIGHT[index].addEventListener('click', () => {
+            count++
+            if (count !== SLIDERS.length) {
+                BUTTON.style.transform = `translateX(-${offset * count}px`;
+            } else {
+                count = 0
+                BUTTON.style.transform = `translateX(0px)`
+            }
+        })
+    })
+
+    BUTTON_LEFT.forEach((element, index) => {
+        BUTTON_LEFT[index].addEventListener('click', () => {
+            count--
+            BUTTON.style.transform = `translateX(-${offset * count}px`;
+        })
+    })
+}
+scheduleList()
+//SLIDER GALLERY
 const sliderGallery = () => {
     const MAIN_GALLERY_LIST = document.querySelectorAll('.gallery__list-item');
     const GALLERY_LIST = document.querySelectorAll('.gallery__slider-item');
@@ -75,7 +170,17 @@ const sliderGallery = () => {
     const BUTTON_LEFT = document.querySelector('.gallery__slider-left');
     const BUTTON_RIGHT = document.querySelector('.gallery__slider-right');
 
+
     let currentIndex = 0;
+
+   if(GALLERY) {
+       GALLERY.addEventListener('click', (e) => {
+           if(e.target === GALLERY) {
+               GALLERY.classList.remove('gallery__slider-show');
+               document.body.style.overflow = '';
+           }
+       })
+   }
 
     MAIN_GALLERY_LIST.forEach((element, index) => {
         element.addEventListener('click', () => {
@@ -94,6 +199,7 @@ const sliderGallery = () => {
     });
 
     BUTTON_RIGHT.addEventListener('click', () => {
+
         GALLERY_LIST[currentIndex].classList.remove('gallery__slider-show');
         currentIndex = (currentIndex + 1) % GALLERY_LIST.length;
         GALLERY_LIST[currentIndex].classList.add('gallery__slider-show');
@@ -105,29 +211,35 @@ const sliderGallery = () => {
         GALLERY_LIST[currentIndex].classList.add('gallery__slider-show');
     });
 };
-
 sliderGallery();
-
+//SLIDER GALLERY MOBILE
 const sliderGalleryMobile = () => {
-    const GALLEY = document.querySelector('.gallery__mobile')
-    const BUTTON = document.querySelector('.program__button-photo')
+    const GALLERY = document.querySelector('.gallery__mobile')
+    const BUTTON = document.querySelector('.program__conditions-button')
     const CLOSE = document.querySelector('.gallery__mobile-close')
+    const BACKGROUND = document.querySelector('.gallery__mobile-background')
+
+    if(BACKGROUND) {
+        BACKGROUND.addEventListener('click', (e) => {
+            if(e.target === BACKGROUND) {
+                GALLERY.classList.remove('gallery__mobile-show');
+                document.body.style.overflow = '';
+            }
+        })
+    }
 
     BUTTON.addEventListener('click', () => {
-        GALLEY.classList.add('gallery__mobile-show')
+        GALLERY.classList.add('gallery__mobile-show')
         document.body.style.overflow = 'hidden'
     })
 
     CLOSE.addEventListener('click', () => {
-        GALLEY.classList.remove('gallery__mobile-show')
+        GALLERY.classList.remove('gallery__mobile-show')
         document.body.style.overflow = ''
     })
-
 }
-
 sliderGalleryMobile()
-
-
+//SLIDER MORE
 const sliderMore = () => {
     const carousel = document.querySelector('.more__slider-list')
     const nextButton = document.querySelector('.next')
@@ -155,48 +267,8 @@ const sliderMore = () => {
         }, 50)
     })
 }
-
 sliderMore()
-
-const buttonOrderClick = (button) => {
-    button.addEventListener('mousedown', () => {
-        button.style.opacity = '0.7';
-    })
-
-    button.addEventListener('mouseup', () => {
-        button.style.opacity = '1';
-    })
-}
-
-buttonOrderClick(BUTTON_ORDER)
-buttonOrderClick(BUTTON_DOWNLOAD)
-buttonOrderClick(BUTTON__PHOTO)
-buttonOrderClick(BUTTON_ORDER_ROUTE)
-
-
-const buttonMenuClick = (button) => {
-    const BACKGROUND_MENU = document.querySelector('.menu__background')
-
-    if (BACKGROUND_MENU) {
-        BACKGROUND_MENU.addEventListener('click', () => {
-            MENU.classList.remove('menu__show')
-            return document.body.style.overflow = '';
-        })
-    }
-
-    button.addEventListener('click', () => {
-        MENU.classList.toggle('menu__show')
-
-        return MENU.classList.contains('menu__show') ?
-            document.body.style.overflow = 'hidden'
-            :
-            document.body.style.overflow = '';
-    })
-}
-
-buttonMenuClick(BURGER)
-buttonMenuClick(CLOSE_MENU)
-
+//BUTTON SELECT
 const buttonSelectClick = (button, toggleClass, field) => {
     const ITEMS = document.querySelector(toggleClass)
     const SELECT_FIELD = document.querySelector(field)
@@ -210,55 +282,27 @@ const buttonSelectClick = (button, toggleClass, field) => {
         return document.querySelector(toggleClass).classList.toggle('menu__select-show')
     })
 }
-
 buttonSelectClick(SELECT_BUTTON_CURRENCY, '.currency__list', '.field__currency')
 buttonSelectClick(SELECT_BUTTON_LANGUAGE, '.language__list', '.field__language')
+//INFORMATION LIST
+const informationList = () => {
+    const INFORMATION_ITEMS = document.querySelectorAll('.information__list-item')
+    const INFORMATION_BUTTONS = document.querySelectorAll('.information__button-item')
+    const INFORMATION_CONTENT = document.querySelectorAll('.information__list-content')
 
 
-INFORMATION_ITEMS.forEach((element, index) => {
+    INFORMATION_ITEMS.forEach((element, index) => {
 
-    element.addEventListener('click', () => {
-        if (element) {
-            INFORMATION_BUTTONS[index].classList.toggle('information__list-button')
-            INFORMATION_CONTENT[index].classList.toggle('information__show')
-        }
+        element.addEventListener('click', () => {
+            if (element) {
+                INFORMATION_BUTTONS[index].classList.toggle('information__list-button')
+                INFORMATION_CONTENT[index].classList.toggle('information__show')
+            }
+        })
     })
-})
-
-SCHEDULE_ITEMS.forEach((element, index) => {
-
-    element.addEventListener('click', () => {
-        if (element) {
-            SCHEDULE_BUTTONS[index].classList.toggle('information__list-button')
-            SCHEDULE_CONTENT[index].classList.toggle('information__show')
-        }
-    })
-})
-
-SCHEDULE_BUTTON.addEventListener('click', () => {
-    const BUTTON_CONTENT = SCHEDULE_BUTTON.innerText
-
-    switch (BUTTON_CONTENT) {
-        case 'Раскрыть все дни' :
-            SCHEDULE_BUTTON.innerText = 'Закрыть все дни'
-
-            SCHEDULE_ITEMS.forEach((element, index) => {
-                SCHEDULE_BUTTONS[index].classList.add('information__list-button')
-                SCHEDULE_CONTENT[index].classList.add('information__show')
-            })
-            break;
-        case 'Закрыть все дни' :
-            SCHEDULE_BUTTON.innerText = 'Раскрыть все дни'
-
-            SCHEDULE_ITEMS.forEach((element, index) => {
-                SCHEDULE_BUTTONS[index].classList.remove('information__list-button')
-                SCHEDULE_CONTENT[index].classList.remove('information__show')
-            })
-            break;
-    }
-})
-
-
+}
+informationList()
+//PRICE SLIDER
 const priceSlider = () => {
     const BUTTON_RIGHT = document.querySelector('#right')
     const BUTTON_LEFT = document.querySelector('#left')
@@ -276,9 +320,8 @@ const priceSlider = () => {
         CARD__LIST.style.transform = `translateX(0px)`;
     });
 }
-
 priceSlider()
-
+//PRICE LIST
 const priceList = () => {
     const BLOCK_PRICE = document.querySelectorAll('.price__lists-top')
     const BUTTON_PRICE = document.querySelectorAll('.price__lists-button')
@@ -294,39 +337,4 @@ const priceList = () => {
         return PRICE_LIST[1].classList.toggle('price__show')
     })
 }
-
 priceList()
-
-
-const scheduleSlider = () => {
-    const SLIDERS = document.querySelectorAll('.schedule__mobile-item')
-    const BUTTON_LEFT = document.querySelectorAll('.schedule__left')
-    const BUTTON_RIGHT = document.querySelectorAll('.schedule__right')
-    const BUTTON = document.querySelector('.schedule__mobile-list')
-
-    let offset = SLIDERS[0].clientWidth
-    let count = 0
-
-
-    BUTTON_RIGHT.forEach((element, index) => {
-        BUTTON_RIGHT[index].addEventListener('click', () => {
-            count++
-            if (count !== SLIDERS.length) {
-                BUTTON.style.transform = `translateX(-${offset * count}px`;
-            } else {
-                count = 0
-                BUTTON.style.transform = `translateX(0px)`
-            }
-        })
-    })
-
-    BUTTON_LEFT.forEach((element, index) => {
-        BUTTON_LEFT[index].addEventListener('click', () => {
-            count--
-            BUTTON.style.transform = `translateX(-${offset * count}px`;
-        })
-    })
-
-}
-
-scheduleSlider()
